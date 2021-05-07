a234c246c2d71e8871a1b275baf832d6913ec11b
Martin Gagnon <mgagnon@mirego.com>
2021-05-06 20:13:57 -0400

    Updated\

--- android/src/main/java/com/mirego/nwad/MainActivity.kt
+++ android/src/main/java/com/mirego/nwad/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.mirego.nwad
 
-import com.mirego.nwad.common.BaseViewModelActivity
-import com.mirego.nwad.databinding.ActivityMainBinding
-import com.mirego.nwad.viewmodels.base.BaseNavigationDelegate
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.mirego.nwad.viewmodels.home.HomeViewModel
 import com.mirego.nwad.viewmodels.home.HomeViewModelController
+import com.mirego.trikot.viewmodels.declarative.controller.NavigationDelegate
+import com.mirego.trikot.viewmodels.declarative.controller.ViewModelActivity
 
-class MainActivity : BaseViewModelActivity<BaseNavigationDelegate, HomeViewModel>() {
+class MainActivity : ViewModelActivity<HomeViewModelController, HomeViewModel, NavigationDelegate>() {
     override val viewModelController: HomeViewModelController by lazy {
         getViewModelController(
             HomeViewModelController::class
         )
     }
 
-    override val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(
-            layoutInflater
-        )
+    @Composable
+    fun composableContent(viewModel: HomeViewModel) {
+        Text(text = "Hellow Android")
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                composableContent(viewModel)
+            }
+        }
     }
 }
