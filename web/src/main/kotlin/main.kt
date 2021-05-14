9584ac3240d0bca781dbbc8fc1a133b9883bddd4
Martin Gagnon <mgagnon@mirego.com>
2021-05-13 20:34:33 -0400

    Add HttpRequestFactory

--- web/src/main/kotlin/main.kt
+++ web/src/main/kotlin/main.kt
@@ -1,4 +1,7 @@
 import com.mirego.nwad.factories.Bootstrap
+import com.mirego.trikot.http.HttpConfiguration
+import com.mirego.trikot.http.web.WebHttpRequest
+import com.mirego.trikot.http.web.WebHttpRequestFactory
 import com.mirego.trikot.streams.cancellable.CancellableManager
 import kotlinx.browser.document
 import react.dom.div
@@ -7,6 +10,7 @@ import react.dom.render
 @ExperimentalJsExport
 fun main() {
     kotlinext.js.require("bootstrap/dist/css/bootstrap.min.css")
+    HttpConfiguration.httpRequestFactory = WebHttpRequestFactory()
 
     render(document.getElementById("root")) {
         div {
